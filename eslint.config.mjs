bad6aae1df23c0247fff59fe3aeb60b01dbe127c
Sujeet kumar <ksujeetkumar7678@gmail.com>
2025-02-02 10:11:09 +0000

    chore: set up project structure with linting, formatting, and environment configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,58 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import checkFile from "eslint-plugin-check-file";
+import n from "eslint-plugin-n";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "prettier",
+  ),
+  {
+    plugins: {
+      "check-file": checkFile,
+      n,
+    },
+    rules: {
+      "prefer-arrow-callback": "error",
+      "prefer-template": "error",
+      semi: "error",
+      quotes: ["error", "double"],
+      "n/no-process-env": "off",
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*.{ts,tsx,js,jsx}": "KEBAB_CASE",
+        },
+        {
+          ignoreMiddleExtensions: true,
+        },
+      ],
+      "check-file/folder-naming-convention": [
+        "error",
+        {
+          "src/**/!^[.*": "KEBAB_CASE",
+        },
+      ],
+      "@typescript-eslint/no-unused-expressions": [
+        "error",
+        {
+          allowShortCircuit: true,
+          allowTernary: true,
+          allowTaggedTemplates: true,
+        },
+      ],
+    },
+  },
+];
+
+export default eslintConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Button } from "@/components/ui/button";
+
+const Page = () => {
+  return (
+    <div>
+      <Button>Hello</Button>
+    </div>
+  );
+};
+
+export default Page;
