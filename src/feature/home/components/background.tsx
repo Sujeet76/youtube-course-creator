51787c6c6bba6e5de6927fabe48cf7b7070eae3f
Sujeet76 <126092124+Sujeet76@users.noreply.github.com>
2025-02-13 17:56:26 +0530

    feat: add mobile detection to background component to hide on mobile devices

--- src/feature/home/components/background.tsx
+++ src/feature/home/components/background.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 
 import Aurora from "@/components/shared/aura-background";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 export const Background = () => {
   const [colorStops, setColorStops] = useState<[string, string, string]>([
@@ -13,6 +14,7 @@ export const Background = () => {
     "#8f37d75c",
   ]);
   const { resolvedTheme } = useTheme();
+  const isMobile = useIsMobile();
 
   useEffect(() => {
     console.log({ resolvedTheme });
@@ -20,6 +22,8 @@ export const Background = () => {
       setColorStops(["#2cd8d55c", "#6b8dd65c", "#8f37d75c"]);
     else setColorStops(["#5D9FFF", "#B8DCFF", "#6BBBFF"]);
   }, [resolvedTheme]);
+
+  if (isMobile) return null;
 
   return <Aurora colorStops={colorStops} speed={0.5} amplitude={1} />;
 };
