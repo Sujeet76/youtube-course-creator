c0f3dc87a73079f166836dff68236586d3336597
Sujeet76 <126092124+Sujeet76@users.noreply.github.com>
2025-02-17 17:31:33 +0530

    fix: update session cookie retrieval for production environment

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,15 @@
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
+import { env } from "process";
+
 export async function middleware(request: NextRequest) {
   const cookie = await cookies();
-  const sessionCookieValue = cookie.get("better-auth.session_token")?.value;
+  const sessionCookieValue = cookie.get(
+    env.NODE_ENV === "production"
+      ? "__Secure-better-auth.session_token"
+      : "better-auth.session_token"
+  )?.value;
   if (!sessionCookieValue) {
     return NextResponse.redirect(new URL("/", request.url));
   }
@@ -11,5 +17,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*"], // Specify the routes the middleware applies to
+  matcher: ["/dashboard/:path*", "/dashboard"], // Specify the routes the middleware applies to
 };
