aeabaf7674d02720e865740e78e8dae88d53a38c
Sujeet76 <126092124+Sujeet76@users.noreply.github.com>
2025-02-19 23:22:02 +0530

    feat: set staleTime to Infinity for video details query

--- src/feature/video-view/api/use-video-details-by-id.tsx
+++ src/feature/video-view/api/use-video-details-by-id.tsx
@@ -3,5 +3,6 @@ import { api } from "@/trpc/client";
 export const useVideoDetailsById = (id: string) => {
   return api.courseView.getVideoById.useSuspenseQuery(id, {
     refetchOnWindowFocus: false,
+    staleTime: Infinity,
   });
 };
